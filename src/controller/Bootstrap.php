50d807c0cc4a814cb57714aa84d00f20eb43f7d4
liugene <750688237@qq.com>
2018-11-29 20:04:53 +0800

    修改命名空间

--- src/controller/Bootstrap.php
+++ src/controller/Bootstrap.php
@@ -7,6 +7,7 @@ class Bootstrap
 
     public function boot($plugin)
     {
+        app()->get(\linkphp\loader\Loader::class)->addNamespacePsr4('addons\\', ROOT_PATH . 'src/addons');
         $plugin = 'addons\\' . $plugin . '\\Plugin' . ucfirst($plugin);
         return call_user_func([new $plugin, 'execute']);
     }
